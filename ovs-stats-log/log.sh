4a9bd409d395f6ea67e9a7c0c70c1acb28767b58
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2017-09-03 20:01:40 +0900

    OvS generate log through crontab

---
+++ ovs-stats-log/log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+FILENAME=`date +%Y%m%d%H%M`.log
+DIR="/home/wide/camp17-docker-nfv/ovs-stats-log"
+
+ovs-ofctl dump-flows vswitch0     > ${DIR}/dump-flows/${FILENAME}
+ovs-ofctl dump-ports vswitch0     > ${DIR}/dump-ports/${FILENAME}
+ovs-ofctl dump-aggregate vswitch0 > ${DIR}/dump-aggregate/${FILENAME}
